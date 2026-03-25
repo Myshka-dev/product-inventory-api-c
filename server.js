69f6efc886756f337355f10c85f5eef9b6e436bd
Myshka-dev <marie_myshka@hotmail.com>
2026-03-25 03:47:07 +0100

    myshka: initial commit: Project structure setup,  with basic express server, initializing environmental variables

---
+++ server.js
@@ -0,0 +1,18 @@
+// This is a simple Express server that listens on port 3000 and responds with "Hello World!" when the root URL is accessed.
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+
+// Define a route for the root URL that sends "Hello World!" as the response
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server and listen on the specified port
+const PORT = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App listening on: http://localhost:${PORT}`);
+});
